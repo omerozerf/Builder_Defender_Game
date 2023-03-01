6f1e40ff31f5a425437c97f6f299c7fa28d380cb
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-03-01 20:53:41 +0300

    2.12 10.10

---
+++ Assets/_Scripts/ResourcesUI.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourcesUI : MonoBehaviour
+{
+    private void Awake()
+    {
+        ResourceTypeListSO resourceTypeList = Resources.Load<ResourceTypeListSO>(nameof(ResourceTypeListSO));
+
+        Transform resourceTemplate = transform.Find("Resource Template");
+        resourceTemplate.gameObject.SetActive(false);
+
+        int index = 0;
+        foreach (ResourceTypeSO resourceType in resourceTypeList.list)
+        {
+            Transform resourceTransform = Instantiate(resourceTemplate, transform);
+            resourceTransform.gameObject.SetActive(true);
+
+            float offsetAmount = -160f;
+            resourceTransform.GetComponent<RectTransform>().anchoredPosition = new Vector2(offsetAmount * index, 0);
+            
+            index++;
+        }
+    }
+}
