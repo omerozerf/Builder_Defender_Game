205c5f51b572f8f09fda1928fc89a0d76e398762
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-03-03 19:08:49 +0300

    2.14 Cinemachine

--- Assets/_Scripts/CameraHandler.cs
+++ Assets/_Scripts/CameraHandler.cs
@@ -9,15 +9,23 @@ public class CameraHandler : MonoBehaviour
     [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
 
     private float orthographicSize;
-    
+    private float targerOrthographicSize;
+
     
     private void Start()
     {
         orthographicSize = cinemachineVirtualCamera.m_Lens.OrthographicSize;
+        targerOrthographicSize = orthographicSize;
     }
 
 
     private void Update()
+    {
+        HandleMovement();
+        HandleZoom();
+    }
+
+    private void HandleMovement()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
@@ -25,9 +33,21 @@ public class CameraHandler : MonoBehaviour
         Vector3 moveDir = new Vector3(x, y);
         float moveSpeed = 30f;
         transform.position += moveDir * (moveSpeed * Time.deltaTime);
+    }
 
+
+    private void HandleZoom()
+    {
         float zoomAmount = 2f;
-        orthographicSize += Input.mouseScrollDelta.y * zoomAmount;
+        targerOrthographicSize += -Input.mouseScrollDelta.y * zoomAmount;
+
+        float minOrthographicSize = 10;
+        float maxOrthographicSize = 30;
+        targerOrthographicSize = Math.Clamp(targerOrthographicSize, minOrthographicSize, maxOrthographicSize);
+
+        float zoomSpeed = 5f;
+        orthographicSize = Mathf.Lerp(orthographicSize, targerOrthographicSize, Time.deltaTime * zoomSpeed);
+        
         cinemachineVirtualCamera.m_Lens.OrthographicSize = orthographicSize;
     }
 }
